a7d176ffc120dc646cad7ed9846d8049a73a0460
sharansudheer <saran_sudheer@hotmail.com>
2024-09-29 23:07:48 +0530

    push:singly_linked_list
    Hashing using Singly Linked List(Chaining)

--- Foundations_of_DSA/Hash_Tables/singly_linked_list.cpp
+++ Foundations_of_DSA/Hash_Tables/singly_linked_list.cpp
@@ -1,6 +1,9 @@
 // https://medium.com/codex/hash-tables-with-singly-linked-lists-in-c-efc3c0166065
 // Singly Linked_List 
+node *head = hash_table[hash_value];
 
+newnode->next = hash_table[hash_value];
+hash_table[hash_value] = newnode;
 
 node *tmp;
 node *cursor;
